1cd1764a728b10dda34579eaaf7adf03d29b0f46
ana <anaranja111@gmail>
2024-10-09 18:21:18 -0500

    Ejercicios JavaScript

---
+++ ejercicios.js
@@ -0,0 +1,104 @@
+/*Ejercicio 1
+let numeroUsuario = prompt("Digita un número")
+let contador=0
+for(let i=1; i<=numeroUsuario;i++){ 
+    contador+=i
+
+};
+console.log(contador)*/
+
+/* Ejercicio 2
+let numeroUsuario = prompt("Digita un número")
+if (numeroUsuario%2===0){
+    console.log("Es par")
+     
+} else{ 
+    console.log("Es impar")
+     
+ }*/
+ 
+/* Ejercicio 3    
+    let generoUsuario = prompt("Ingrese su género F/M").toLowerCase()
+    let edadUsuario = prompt("Ingrese su edad:")
+    if(generoUsuario==="f" && edadUsuario>18 ) {
+        console.log("Recibes una porción de pizza hawaiana y una cerveza");
+            } else if (generoUsuario==="f" && edadUsuario<=10)  {
+            
+                console.log("Recibes una porción de pizza hawaiana y un jugo")
+            }
+                else if (generoUsuario==="m" && edadUsuario<=10){
+                    console.log("Recibes una porción de pizza de tres carnes y un jugo");
+
+                } else if (generoUsuario==="m" && edadUsuario>18){
+                    console.log("Recibes una porción de pizza de tres carnes y una cerveza");
+                    
+                } else{
+                    console.log("No recibes ningún premio :c");
+                    
+                } */
+
+// Ejercicio 4                   
+                   
+let menu = {"Hamburguesa de falafel......":20.000,
+    "Tacos":15000,
+    "Orchata":8000,
+    "Limonada":8000,
+    "Pie de limón":10000,
+}
+
+prompt(`Escoge tu menú:${menu}`); 
+               
+                    
+ // Ejercicio 5
+ 
+ /*
+  const num1 = prompt("Digita un número") ;
+  const num2=prompt("¿Hasta qué número quieres multiplicarlo?");
+  
+   
+  let total=0;
+  for(let i=1; i<= num2; i++){
+    const resultado=num1*i
+    console.log(`${num1} x ${i} = ${resultado}`);
+   
+    total+=resultado ;
+  
+    
+}
+
+console.log(`la suma de los resultados es: ${total}`);
+  */
+
+//Ejercicio 6
+
+
+
+
+
+/*
+let matricula=10000000
+let promedio= prompt("Ingrese su promedio:");
+let pagar = 0;
+if (promedio < 3){
+    
+    pagar=matricula
+    ;
+
+}  
+
+    else if (promedio>=3 && promedio<=4){
+        pagar=matricula * 0.95;
+      
+
+    }
+    else if (promedio>4){
+        pagar=matricula * 0.50;
+
+        
+
+    }
+
+    console.log(`El valor de matrícula a pagar es: ${pagar}`);
+
+
+*/
